a32921701bc6a1353a6624eff81cd8caed7603ce
Rachel Ann Shulkin <73843433+rshulkin@users.noreply.github.com>
2022-04-18 23:10:24 -0400

    more

---
+++ finalprojectapple.py
@@ -0,0 +1,99 @@
+import sqlite3
+import requests
+import json
+import jwt
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime, timedelta
+url = "https://api.music.apple.com/v1/catalog/US/charts"
+key_id = "688Z3NH7KW"
+team_id = "A243U6Q6LD"
+secret_key = open("AuthKey_688Z3NH7KW.p8").read()
+token_str = None
+token_valid_until = None
+
+def generate_token(session_length=12):
+    """
+    Generate encrypted token to be used by in API requests.
+    Set the class token parameter.
+    :param session_length: Length Apple Music token is valid, in hours
+    """
+    global token_str, token_valid_until
+    token_exp_time = datetime.now() + timedelta(hours=session_length)
+    headers = {
+        'alg': "ES256",
+        'kid': key_id,
+    }
+    payload = {
+        'iss': team_id,  # issuer
+        'iat': int(datetime.now().timestamp()),  # issued at
+        'exp': int(token_exp_time.timestamp())  # expiration time
+    }
+    token_valid_until = token_exp_time
+    token = jwt.encode(payload, secret_key, algorithm="ES256", headers=headers)
+    token_str = token if type(token) is not bytes else token.decode()
+    open("token.str", "w").write(token_str)
+    open("token.exp", "w").write(str(token_valid_until.timestamp()))
+
+
+
+if "token.str" in os.listdir():
+    token_valid_until = datetime.fromtimestamp(float(open("token.exp").read()))
+
+if token_valid_until is None or datetime.now() >= token_valid_until:
+    generate_token()
+else:
+    token_str = open("token.str").read()
+
+# print(token_str)
+
+headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token_str}"}
+max_songs = 100
+response = requests.get(url, headers=headers, params={"types": "songs", "chart": "most-played", "limit": max_songs})
+print(response.json())
+
+# using i as an iterating index
+# response.json()["results"]["songs"][0]["data"][i]["attributes"]["name"]
+# response.json()["results"]["songs"][0]["data"][i]["attributes"]["artistName"]
+
+con = sqlite3.connect("music.db")
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS apple_music_songs 
+               (id INTEGER PRIMARY KEY, rank INT, song TEXT, artist_id INT);''')
+cur.execute('''CREATE TABLE IF NOT EXISTS apple_music_artists
+               (id INTEGER PRIMARY KEY, name TEXT UNIQUE);''')
+songs_saved = 0 
+for i in range(max_songs):
+    song_name = response.json()["results"]["songs"][0]["data"][i]["attributes"]["name"]
+    artist_name = response.json()["results"]["songs"][0]["data"][i]["attributes"]["artistName"] 
+    cur.execute("INSERT OR IGNORE INTO apple_music_artists (name) VALUES (?);", (artist_name,))
+    cur.execute("SELECT id FROM apple_music_artists WHERE name = ?;", (artist_name,))
+    artist_id = cur.fetchone()[0]
+    cur.execute("SELECT * FROM apple_music_songs WHERE song = ?;", (song_name,))
+    if not cur.fetchone():
+        cur.execute("INSERT INTO apple_music_songs (rank, song, artist_id) VALUES (?, ?, ?);", (i+1, song_name, artist_id))
+        songs_saved += 1
+    if songs_saved >= 25:
+        break
+
+cur.execute("SELECT name, COUNT(apple_music_songs.id) AS songs_in_apple_top100 FROM apple_music_artists JOIN apple_music_songs ON artist_id = apple_music_artists.id GROUP BY apple_music_artists.id ORDER BY songs_in_apple_top100 ASC;")
+apple_top_artists = cur.fetchall()
+
+con.commit()
+con.close()
+
+results = dict(apple_top_artists)
+
+objects = results.keys()
+y_pos = np.arange(len(objects))
+performance = results.values()
+plt.rc("ytick", labelsize = 6)
+plt.barh(y_pos, performance, align='center', color = "red")
+plt.yticks(y_pos, objects)
+plt.xticks(np.arange(21))
+plt.xlabel("Number of Songs in Apple's Top 100")
+plt.ylabel('Artists')
+plt.title("Top Artists on Apple Music's Chart")
+
+plt.show()
